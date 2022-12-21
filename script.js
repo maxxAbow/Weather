3399dcbd298d3650a70f3256cafdac69c363b59b
maxxAbow <mbowman@welland.com>
2022-12-20 21:09:45 -0500

    set default location

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-//Variables
+// //Variables
 var location = document.getElementById('searchParam');
-var apiKey = 'appid=ec34c45751e37747cd458e98fe53cb22'
-var dataUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&' + 'limit=3&' + apiKey;
+var apiKey = 'e62f9765160953549b7bdae64e63ff92';
+var dataUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=3&appid=' + apiKey;
 
-var longitude = dataUrl.lon;
-var latitude = dataUrl.lat;
+// // Set location default to Demo
+function defaultLocation (){
+    if (location === ''){
+        location.innerHTML = 'Demo'
+    }
+};
 
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appkey=' + apiKey;
-
+// //Grab lat/lon based on location
+// var longitude = dataUrl.lon;
+// var latitude = dataUrl.lat;
+// //Grab weather based on lat/lon
+// var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+// //Insert weather to searchResults
 var forecast = document.getElementById('searchResults');
-
 forecast.innerHTML=weatherUrl;
