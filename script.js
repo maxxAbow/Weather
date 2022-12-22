58cac256f8dbd8a88e306ba7382aaca1e891670a
maxxAbow <mbowman@welland.com>
2022-12-21 19:47:13 -0500

    attempt displaying Demo weather

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 // //Variables
-var location = document.getElementById('searchParam');
-var apiKey = 'e62f9765160953549b7bdae64e63ff92';
-var dataUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=3&appid=' + apiKey;
 
-// // Set location default to Demo
+// var location = document.getElementById('searchParam');
+var forecast = document.getElementById('searchResults');
+var apiKey = 'e62f9765160953549b7bdae64e63ff92';
+// var dataUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=3&appid=' + apiKey;
+var dataUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=demo&appid=e62f9765160953549b7bdae64e63ff92';
+
+// // // Set location default to Demo
 function defaultLocation (){
     if (location === ''){
         location.innerHTML = 'Demo'
     }
 };
 
-// //Grab lat/lon based on location
-// var longitude = dataUrl.lon;
-// var latitude = dataUrl.lat;
-// //Grab weather based on lat/lon
-// var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
-// //Insert weather to searchResults
-var forecast = document.getElementById('searchResults');
-forecast.innerHTML=weatherUrl;
+// // //Grab lat/lon based on location
+fetch(dataUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var latitude = data[0].lat;
+    console.log(data[0].lat);
+    var longitude = data[0].lon
+    console.log(data[0].lon);
+
+// // //Grab weather based on lat/lon
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+fetch(weatherUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (secondData){
+// // //Insert weather to searchResults
+    forecast.innerHTML=secondData.list.weather.icon;
+    console.log(secondData.list.weather);
+    })
+})
